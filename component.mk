174f8083d88fd44f1862e2cf0680efcee6ea93b2
Hai Pham <bienxanhhaiduong@gmail.com>
2022-08-17 10:23:53 +0700

    remove test

--- component.mk
+++ component.mk
@@ -1,4 +1,4 @@
 ESP_IDF=${IDF_PATH} 
-COMPONENT_ADD_INCLUDEDIRS := include ${ESP_IDF}/tools/unit-test-app/components/unity/include/ library/ccronexpr library/jobs .
-COMPONENT_SRCDIRS :=  library/ccronexpr library/jobs test src .
+COMPONENT_ADD_INCLUDEDIRS := include library/ccronexpr library/jobs .
+COMPONENT_SRCDIRS := library/ccronexpr library/jobs .
 CFLAGS += -D CRON_USE_LOCAL_TIME
